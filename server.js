34a71397af545d292516a3ce36302dd8f03b9e00
Shang <yopshang@gmail.com>
2022-03-06 14:22:55 +0800

    新增：server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+const {v4: uuidv4} = require('uuid');
+var http = require('http')
+const todos = [];
+
+const requestListener = (req, res) => {
+    // console.log(req.url);
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }
+    let body = '';
+    req.on('data', chunk=>{
+        body+=chunk
+    })
+
+    if(req.url == "/todos" && req.method == "GET"){
+        req.on('end', ()=>{
+            console.log(JSON.parse(body));
+            res.writeHead(200, headers);
+            res.write(JSON.stringify({
+                "status": "success",
+                "data": todos
+            }));
+            res.end();
+        })
+    } else if(req.url == "/todos" && req.method == "POST"){
+        req.on('end', ()=>{
+            // console.log(JSON.parse(body));
+            const title = JSON.parse(body).title;
+            console.log('post title:', title);
+            res.writeHead(200, headers);
+            res.write(JSON.stringify({
+                "status": "success",
+                "data": todos
+            }));
+            res.end();
+        })
+    }
+    else if (req.method == "OPTIONS"){
+        res.writeHead(200, headers);
+        res.end();
+    } else {
+        res.writeHead(404, headers);
+        console.log('404');
+        res.write(JSON.stringify({
+            "status": "false",
+            "message": "無此網站路由"
+        }));
+        res.end();
+    }
+}
+const server = http.createServer(requestListener);
+server.listen(3005);
